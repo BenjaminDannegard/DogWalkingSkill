551211ddad6b9ce1733fc9e96da8a0938ec1b023
BenjaminDannegard <benjamin.dannegard@gmail.com>
2019-01-04 14:19:02 +0100

    Git em

--- __init__.py
+++ __init__.py
@@ -10,23 +10,26 @@ class DogWalkingSkill(MycroftSkill):
     def __init__(self):
         super(DogWalkingSkill, self).__init__("DogWalkingSkill")
 
-    def initialize(self):
-        # Creating GreetingsIntent requiring Greetings vocab
-        wweather = IntentBuilder("WWeatherIntent").require("query").require("WWeather").build()
-        # Associating a callback with the Intent
-        self.register_intent(wweather, self.handle_wweather)
-        walk = IntentBuilder("WalkIntent").require("query").require("Walk1").build()
-        self.register_intent(walk, self.handle_walk)
+    # def initialize(self):
+    #     # Creating GreetingsIntent requiring Greetings vocab
+    #     wweather = IntentBuilder("WWeatherIntent").require("query").require("WWeather").build()
+    #     # Associating a callback with the Intent
+    #     self.register_intent(wweather, self.handle_wweather)
+    #     walk = IntentBuilder("WalkIntent").require("query").require("Walk1").build()
+    #     self.register_intent(walk, self.handle_walk)
 
-        
+    @intent_handler(IntentBuilder("WWeatherIntent").require("query").require("WWeather").build())
         
     def handle_wweather(self):
-        # Sending a command to mycroft, speak Greetings Dialog
         self.speak_dialog("WWeather")
+
+
+    @intent_handler(IntentBuilder("WalkIntent").require("query").require("Walk").build())
 
     def handle_walk(self):
         self.speak_dialog("Walking")
         
+   
     def stop(self):
         pass
 
